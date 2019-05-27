f230aa3aac14883ec53d31620fc5dd9449e3cef8
Mansur <Mansur@DESKTOP-6J84VFU>
2019-05-27 15:44:02 +0500

    App.config changed

--- NumberSystemTestingApp/Program.cs
+++ NumberSystemTestingApp/Program.cs
@@ -13,7 +13,17 @@ namespace NumberSystemTestingApp
         static void Main(string[] args)
         {
             string path = Environment.CurrentDirectory + @"\";
-            string dllFileName = ConfigurationSettings.AppSettings["DllFile"];
+            var dllFileName = ConfigurationSettings.AppSettings["DllFile"];
+
+            if (dllFileName == null)
+                throw new Exception("Dll file not found");
+
+            var firstNumber = ConfigurationSettings.AppSettings["FirstNumber"];
+            var secondNumber = ConfigurationSettings.AppSettings["SecondNumber"];
+            var numberSystem = ConfigurationSettings.AppSettings["Type"];
+            if (firstNumber == null || secondNumber == null || numberSystem == null)
+                throw new Exception("Given data error");
+
             string fileForLoad = path + dllFileName;
             if (!File.Exists(fileForLoad))
                 throw new Exception("File not exists!");
@@ -25,10 +35,13 @@ namespace NumberSystemTestingApp
             if (myServiceType == null)
                 throw new Exception("Service not found");
 
-            INumberSystem bin = Activator.CreateInstance(myServiceType) as INumberSystem;
-
-            Console.WriteLine(bin.Add("101", "11"));
-            Console.ReadLine();
+            var bin = Activator.CreateInstance(myServiceType) as INumberSystem;
+            Console.WriteLine(numberSystem);
+            Console.WriteLine(firstNumber+"+"+secondNumber + " = " + bin.Add(firstNumber, secondNumber));
+            Console.WriteLine(firstNumber + "*" + secondNumber + " = " + bin.Multiply(firstNumber, secondNumber));
+            Console.WriteLine(firstNumber + "-" + secondNumber + " = " + bin.Subtract(firstNumber, secondNumber));
+            Console.WriteLine(secondNumber + "-" + firstNumber + " = " + bin.Subtract(secondNumber, firstNumber));
+            Console.ReadKey();
         }
     }
 }
